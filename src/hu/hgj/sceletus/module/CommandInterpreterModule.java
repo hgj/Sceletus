a4b72848ff53802a624fa5e31dc98a4f8a4a0e20
hgj <hogejo@gmail.com>
2015-11-06 14:37:36 +0000

    Improvement (more helpers) for CommandInterpreterModule

--- src/hu/hgj/sceletus/module/CommandInterpreterModule.java
+++ src/hu/hgj/sceletus/module/CommandInterpreterModule.java
@@ -5,6 +5,7 @@ import hu.hgj.sceletus.queue.WithTopic;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 public abstract class CommandInterpreterModule<T> extends MultiConverterModule<T, List<String>, T, String> {
@@ -33,18 +34,74 @@ public abstract class CommandInterpreterModule<T> extends MultiConverterModule<T
 		super(name, inputQueues, inputQueueFilters, outputQueues);
 	}
 
+	/**
+	 * Convert commands to "standard" format: Lower case and remove {@code -} or
+	 * {@code _} characters.
+	 *
+	 * @param commandName The original command name.
+	 *
+	 * @return The converted command name.
+	 */
+	public static String filterCommandName(String commandName) {
+		return commandName.toLowerCase().replaceAll("[-_]", "");
+	}
+
+	protected List<String> getSupportedCommands() {
+		return Collections.emptyList();
+	}
+
+	protected boolean filterCommand(String command) {
+		String filteredCommandName = filterCommandName(command);
+		for (String supportedCommand : getSupportedCommands()) {
+			if (filteredCommandName.equals(supportedCommand)) {
+				return true;
+			}
+		}
+		logger.debug("Command '{}' does not seem to be supported.", command);
+		return false;
+	}
+
+	@Override
+	protected Predicate<T> getDefaultInputQueueFilter() {
+		List<String> supportedCommands = getSupportedCommands();
+		if (supportedCommands == null || supportedCommands.isEmpty()) {
+			return super.getDefaultInputQueueFilter();
+		} else {
+			return topic -> {
+				String commandName = null;
+				try {
+					Map<String, Object> topicMap = (Map<String, Object>) topic;
+					commandName = (String) topicMap.get("command");
+				} catch (ClassCastException ignored) {
+					commandName = topic.toString();
+				}
+				return filterCommand(commandName);
+			};
+		}
+	}
+
 	public abstract String interpretCommand(List<String> arguments);
 
 	public WithTopic<T, String> interpretCommandWithTopic(WithTopic<T, List<String>> argumentsWithTopic) {
-		return new WithTopic<>(
-				argumentsWithTopic.topic,
-				interpretCommand(argumentsWithTopic.element)
-		);
+		String result = interpretCommand(argumentsWithTopic.element);
+		if (result != null) {
+			return new WithTopic<>(
+					argumentsWithTopic.topic,
+					result
+			);
+		} else {
+			return null;
+		}
 	}
 
 	@Override
 	protected List<WithTopic<T, String>> convertElement(WithTopic<T, List<String>> inputElementWithTopic) {
-		return Collections.singletonList(interpretCommandWithTopic(inputElementWithTopic));
+		WithTopic<T, String> withTopicResult = interpretCommandWithTopic(inputElementWithTopic);
+		if (withTopicResult != null) {
+			return Collections.singletonList(withTopicResult);
+		} else {
+			return Collections.emptyList();
+		}
 	}
 
 }
